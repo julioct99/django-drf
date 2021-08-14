a3ceccfb0597c550691f678cc15ed418c32f08d3
julioct99 <jcasallot01@gmail.com>
2021-08-14 16:23:45 +0200

    1: Serialization

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,3 +1,8 @@
 from rest_framework import serializers
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ["id", "title", "code", "linenos", "language", "style"]
